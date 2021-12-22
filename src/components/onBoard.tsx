e21d363104dc5cfaa744a3eca40a9892cc551470
AnggaWikaNugraha <anggawika18@gmail.com>
2021-12-22 14:55:35 +0700

    onboard

--- src/components/onBoard.tsx
+++ src/components/onBoard.tsx
@@ -1,63 +1,108 @@
-import React from 'react'
+
+import * as React from 'react';
 import { IPropsOnboard } from '../type'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import ILogo from '../assets/images/logo.png';
+import IonboardRs from '../assets/images/oboardRs.png';
 
 const OnBoard = (props: IPropsOnboard) => {
     return (
-        <div className="wrapper index">
-            <div className="ag-header"></div>
-            <div className="ag-main">
-                <section className="login-wrapper">
-                    <div className="login-header">
-                        <img src={require('../assets/images/ag-logo.png').default} alt="" />
-                        <p className="login-title">Konsultasi vidio</p>
-                        <p className="login-subtitle">Brawijaya conferance</p>
-                    </div>
-                    <div className="login-body">
-                        <div className="columns">
-                            <div className="column">
-                                <div className="channel-wrapper control has-icons-left">
-                                    <input
-                                        disabled
-                                        value={props.name ? props.name : ''}
-                                        id="channel"
-                                        className={'ag-rounded input '}
-                                        type="text"
-                                        placeholder='Room name here ..' />
-                                    <span className="icon is-small is-left">
-                                        <img src={require('../assets/images/ag-login.png').default} alt="" />
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="columns">
-                            <div className="column">
-                                <div id="attendeeMode" className="control">
-                                    <label className="radio">
-                                        <input
-                                            value="video" type="radio"
-                                            name="attendee" defaultChecked />
-                                        <span className="radio-btn">
-                                        </span>
-                                        <span className="radio-img video">
-                                        </span>
-                                        <span className="radio-msg">Video Call : join with video call</span>
-                                    </label>
-                                    <br />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="login-footer">
-                        <button
-                            disabled={!props.state.isBtn}
-                            onClick={(e) => props.actCall(e)}
-                            id="joinBtn" className="ag-rounded button is-info">Join
-                        </button>
-                    </div>
-                </section>
-            </div>
-        </div >
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid  container spacing={2}>
+                <WrapperKiri item xs={5}>
+                    <Logo src={ILogo}/>
+                    <OnBoardRs src={IonboardRs}/>
+                </WrapperKiri>
+                <WrapperKanan item xs={7}>
+                    <Title Fw={900} Fc='#000' Fs='32px'>Conference</Title>
+                    <Gap h='10px'/>
+                    <Title Fw={900} Fc='#bdbfc4' Fs='28px'>Join a new conference</Title>
+                    <Gap h='100px'/>
+                    <Title Fw={900} Fc='#000' Fs='28px'>Room</Title>
+                    <Input>
+                        <Title Fw={900} Fc='#bdbfc4' Fs='22px'>{props.name ? props.name : 'No Room !!'}</Title>
+                    </Input>
+                    <Button
+                        disabled={!props.state.isBtn}
+                        onClick={(e) => props.actCall(e)}
+                    >Join</Button>
+                </WrapperKanan>
+            </Grid>
+        </Box>
     )
 }
 
 export default OnBoard
+
+type IGap = {
+    w? : string,
+    h? : string
+}
+
+type ITitle = {
+    Fs? : string,
+    Fc? : string,
+    Fw? : number
+}
+
+const WrapperKiri = styled(Grid)((props) => ({
+    minHeight : '100vh',
+    display: 'flex',
+    alignItems : 'center',
+    flexDirection : 'column'
+}))
+
+const WrapperKanan = styled(Grid)((props) => ({
+    backgroundColor : '#f4f5f9',
+    minHeight : '100vh',
+    display: 'flex',
+    justifyContent : 'center',
+    flexDirection : 'column',
+}))
+
+const Logo = styled('img')((props) => ({
+    width: '25%',
+    marginTop : '125px',
+    marginBottom : '100px'
+}))
+const OnBoardRs = styled('img')((props) => ({
+    width: '100%'
+}))
+
+const Title = styled('div')((props : ITitle)=> ({
+    fontSize : props.Fs ? props.Fs : '14px',
+    color: props.Fc ? props.Fc : 'white',
+    fontWeight : props.Fw ? props.Fw :  400
+}))
+
+const Gap = styled('div')((props : IGap) => ({
+    width: props.w ? props.w : '0px',
+    height: props.h ? props.h : '0px'
+}))
+
+const Input = styled('div')((props)=> ({
+    width: '50%',
+    height: '70px',
+    border: 'none',
+    marginTop : '10px',
+    borderRadius : '10px',
+    paddingLeft: '20px',
+    backgroundColor : 'white',
+    position:  'relative',
+    display: 'flex',
+    alignItems : 'center'
+}))
+
+const Button = styled('button')((props)=> ({
+    marginTop : '30px',
+    backgroundColor : '#004aad',
+    width: '10%',
+    height: '50px',
+    borderRadius : '10px',
+    color : 'white',
+    fontWeight : 'bold',
+    fontSize : '18px',
+    cursor: 'pointer'
+}))
